24160b95dc31e944fbef5fe4abecd1281787e55d
Peter Banda <tremor11@gmail.com>
2019-05-21 10:16:37 +0200

    Dynamic case class instantiation split.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# InCal Core [![version](https://img.shields.io/badge/version-0.1.5-green.svg)](https://in-cal.org)
+# InCal Core [![version](https://img.shields.io/badge/version-0.1.6-green.svg)](https://in-cal.org)
 
 Core library of all the InCal projects containing utility classes, repo interfaces, and shared/common models.
 
@@ -7,7 +7,7 @@ Core library of all the InCal projects containing utility classes, repo interfac
 All you need is **Scala 2.11**. To pull the library you have to add the following dependency to *build.sbt*
 
 ```
-"org.in-cal" %% "incal-core % "0.1.5"
+"org.in-cal" %% "incal-core % "0.1.6"
 ```
 
 or to *pom.xml* (if you use maven)
@@ -16,6 +16,6 @@ or to *pom.xml* (if you use maven)
 <dependency>
     <groupId>org.in-cal</groupId>
     <artifactId>incal-core_2.11</artifactId>
-    <version>0.1.5</version>
+    <version>0.1.6</version>
 </dependency>
 ```
--- src/main/scala/org/incal/core/util/ReflectionUtil.scala
+++ src/main/scala/org/incal/core/util/ReflectionUtil.scala
@@ -1,8 +1,6 @@
 package org.incal.core.util
 
 import java.lang.reflect.InvocationTargetException
-
-import ReflectionUtil._
 import java.{lang => jl}
 
 import scala.collection.Traversable
@@ -49,8 +47,16 @@ object ReflectionUtil {
     instance: T,
     mirror: Mirror = defaultMirror
   ): Traversable[(String, Any)] = {
-    val instanceMirror = mirror.reflect(instance)
     val members = getCaseClassMemberMethods[T]
+    getFieldNamesAndValues(instance, members, mirror)
+  }
+
+  def getFieldNamesAndValues[T: ClassTag](
+    instance: T,
+    members: Traversable[MethodSymbol],
+    mirror: Mirror = defaultMirror
+  ): Traversable[(String, Any)] = {
+    val instanceMirror = mirror.reflect(instance)
 
     members.map { member =>
       val fieldMirror = instanceMirror.reflectField(member.asTerm)
@@ -166,142 +172,6 @@ object ReflectionUtil {
       case x: Int => new jl.Integer(x)
       case x: Long => new jl.Long(x)
       case _ => throw new IllegalArgumentException(s"Don't know how to box $value of type ${value.getClass.getName}.")
-    }
-}
-
-trait DynamicConstructor[E] {
-  def apply(fieldNameValues: Map[String, Any]): Option[E]
-  def apply(valuesInOrder: Seq[Any]): Option[E]
-}
-
-trait DynamicConstructorFinder[E] {
-
-  def apply(
-    fieldNames: Seq[String],
-    typeValueConverters: Traversable[(ru.Type, Any => Any)] = Nil
-  ): Option[DynamicConstructor[E]]
-
-  def classSymbol: ClassSymbol
-}
-
-object DynamicConstructorFinder {
-
-  def apply[E: TypeTag]: DynamicConstructorFinder[E] =
-    new DynamicConstructorFinderImpl[E](typeOf[E])
-
-  def apply[E](className: String): DynamicConstructorFinder[E] =
-    new DynamicConstructorFinderImpl[E](classNameToRuntimeType(className))
-
-  def apply[E](typ: Type): DynamicConstructorFinder[E] =
-    new DynamicConstructorFinderImpl[E](typ)
-}
-
-private class DynamicConstructorFinderImpl[E](runtimeType: ru.Type) extends DynamicConstructorFinder[E] {
-
-  private val defaultTypeValues = Map[Type, Any](
-    typeOf[Option[_]] -> None,
-    typeOf[Boolean] -> false,
-    typeOf[Seq[_]] -> Nil,
-    typeOf[Set[_]] -> Set(),
-    typeOf[Map[_, _]] -> Map()
-  )
-
-  override val classSymbol = runtimeType.typeSymbol.asClass
-  private val cm = classMirror(classSymbol)
-
-  private val constructorsWithInfos = runtimeType.decl(ru.termNames.CONSTRUCTOR).asTerm.alternatives.map{ ctor =>
-    val constructor = cm.reflectConstructor(ctor.asMethod)
-
-    val paramNameAndTypes = ctor.asMethod.paramLists.map(_.map{x => (shortName(x), x.info)}).flatten
-
-    val paramNameDefaultValueMap: Map[String, Any] = paramNameAndTypes.map { case (paramName, paramType) =>
-      val defaultValueOption = defaultTypeValues.find {
-        case (defaultType, defaultValue) => paramType <:< defaultType
-      }.map(_._2)
-      defaultValueOption.map( defaultValue => (paramName, defaultValue))
-    }.flatten.toMap
-
-    (constructor, paramNameAndTypes, paramNameDefaultValueMap)
-  }.sortBy(-_._2.size)
-
-  // chooses the first constructor (the one that satisfies the most parameters...see sorting in the declaration);
-  // alternatively could throw an exception or log a warning saying that multiple constructors could be applied
-  override def apply(
-    fieldNames: Seq[String],
-    typeValueConverters: Traversable[(ru.Type, Any => Any)]
-  ): Option[DynamicConstructor[E]] = {
-    val constructorWithInfosOption = constructorsWithInfos.find { case (constructor, paramNameAndTypes, paramNameDefaultValueMap) =>
-      paramNameAndTypes.forall { case (paramName, _) =>
-        fieldNames.contains(paramName) || paramNameDefaultValueMap.contains(paramName)
-      }
-    }
-
-    constructorWithInfosOption.map { case (constructor, paramNameAndTypes, paramNameDefaultValueMap) =>
-        new DynamicConstructorImpl[E](constructor, paramNameAndTypes, paramNameDefaultValueMap, classSymbol, fieldNames, typeValueConverters)
-    }
-  }
-}
-
-private class DynamicConstructorImpl[E](
-    constructor: MethodMirror,
-    paramNameAndTypes: List[(String, ru.Type)],
-    paramNameDefaultValueMap: Map[String, Any],
-    reflectedClass: ClassSymbol,
-    fieldNames: Seq[String],
-    typeValueConverters: Traversable[(ru.Type, Any => Any)]
-  ) extends DynamicConstructor[E] {
-
-  private lazy val fieldConstructorIndeces = {
-    val paramNameIndexMap = paramNameAndTypes.map(_._1).zipWithIndex.toMap
-    fieldNames.map(paramNameIndexMap.get(_).get)
-  }
-
-  private lazy val constructorValues = paramNameAndTypes.map{ case (paramName, _) =>
-    if (!fieldNames.contains(paramName)) {
-      // failover to default values (we know it exists due to the search performed above)
-      paramNameDefaultValueMap.get(paramName).get
-    } else
-      None
-    }.toSeq
-
-  def apply(fieldNameValueMap: Map[String, Any]): Option[E] =
-    try {
-      val constructorValues = paramNameAndTypes.map { case (paramName, paramType) =>
-        fieldNameValueMap.get(paramName).map { value =>
-          // convert the value if needed
-          typeValueConverters.find(_._1.=:=(paramType)).map { case (_, converter) =>
-            converter(value)
-          }.getOrElse(
-            value
-          )
-        }.getOrElse {
-          // failing over to default values
-          paramNameDefaultValueMap.get(paramName).getOrElse(
-            throw new IllegalArgumentException(s"Constructor of ${reflectedClass.fullName} expects mandatory param '${paramName}' but the result set contains none.")
-          )
-        }
-      }
-      Some(
-        constructor(constructorValues: _*).asInstanceOf[E]
-      )
-    } catch {
-      case e: Exception => {
-//        logger.error(s"Dynamic constructor of ${reflectedClass.fullName} invocation failed.", e)
-        None
-      }
-    }
-
-  override def apply(valuesInOrder: Seq[Any]): Option[E] =
-    if (fieldNames.size != valuesInOrder.size)
-      None
-    else {
-      val newValues: scala.collection.mutable.Seq[Any] = scala.collection.mutable.ArraySeq(constructorValues:_*)
-      (fieldConstructorIndeces, valuesInOrder).zipped.map{ (index, value) =>
-        newValues.update(index, value)
-      }
-      Some(
-        constructor(newValues: _*).asInstanceOf[E]
-      )
     }
 }
 
